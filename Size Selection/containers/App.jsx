9520061da0fb557a15ad52a84de2dc442b72713e
srikanthUIDev <srikanthui12@gmail.com>
2017-10-01 20:33:45 -0500

    pushing final version

---
+++ Size Selection/containers/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import ChoiceBox from '../components/ChoiceBox';
+import Results from '../components/Results';
+
+import {fetchShirts, selectSize} from '../actions';
+
+export class App extends React.Component {
+  componentDidMount(){
+    // Initial Shirt Fetch!
+    this.props.dispatch(fetchShirts(this.props.selectedSize));
+  }
+  render(){
+    return  (
+      <div>
+        <h1>Select a Shirt Size</h1>
+        <ChoiceBox selectedSize={this.props.selectedSize}
+                   selectSize={ size=> 
+                     this.props.dispatch(selectSize(size))}
+        />
+        <Results images={this.props.shirts} />
+      </div>
+    )
+  }
+}
+
+App.defaultProps = {
+  dispatch : function(){}  //noop
+}
+
+function select(state){
+  return state;
+}
+
+export default connect(select)(App);
